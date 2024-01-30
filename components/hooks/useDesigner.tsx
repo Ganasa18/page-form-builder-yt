037b5969818cd5c8e92c78b2f94266c131decb7c
Ganasa18 <126744589+yosuanetzme18@users.noreply.github.com>
2024-01-30 19:18:31 +0700

    Add - initial learn setup

---
+++ components/hooks/useDesigner.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { useContext } from "react";
+import { DesignerContext } from "../context/DesignerContext";
+
+export const useDesigner = () => {
+  const context = useContext(DesignerContext);
+  if (!context) {
+    throw new Error("useDesigner must be used within a DesignerContext");
+  }
+  return context;
+};
